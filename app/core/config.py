73222292f3123694c5296e1d8bd8760d9b8a5b8b
Shashwath-K <shashwathkukkunoor@outlook.com>
2026-03-21 01:13:13 +0530

    CI-03: Enpoints updated

--- app/core/config.py
+++ app/core/config.py
@@ -2,7 +2,7 @@ import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "Ghost-Typer Auto-Documenter"
+    PROJECT_NAME: str = "Semantic File Converter & Autodocumenter"
     HOST: str = "127.0.0.1"
     PORT: int = 8000
     LLM_PROVIDER: str = "ollama"  # Options 'ollama' or 'dummy'
